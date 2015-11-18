87dd58a75d525ea003c837113f725725bbd58713
Hiren Joshi <hirenj@gmail.com>
2015-11-18 08:38:22 +0100

    More skeleton for package

---
+++ R/ggsequence.R
@@ -0,0 +1,10 @@
+#' Produce ggplots for sequence alignments
+#'
+#' Wrap sequence alignment (and re-alignment) methods to
+#' allow for much easier plotting of protein (or general sequence)
+#' features
+#'
+#' @name ggsequence
+#' @author Hiren Joshi <\email{ggsequence@@glycocode.com}>
+#' @docType package
+NULL
