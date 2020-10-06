6e1d901b7d7847eea6bea33fccd1999a49c1d5fa
Hiren Joshi <hirenj@gmail.com>
2020-10-06 21:58:31 +0200

    Fix for drawing test plot

--- R/get_plot.R
+++ R/get_plot.R
@@ -1,15 +1,15 @@
 get_plot = function() {
-
+	require(ggsugar)
 	site_data = data.frame(seq.ids=c('bar','foo'),site=c(4,5),sugar=c('galnac','gal(b1-3)galnac'))
 
 	sigpep_data = data.frame(seq.ids=c('bar','foo'),start=c(1,1),end=c(3,4))
 
 	plot = 	ggplot(do_alignment(c(foo='MNTTTMMMNPPPPMNTTTMMMNPPPPMNTTTMMMNPPPPMNTTTMMMNPPPP',bar='NNSMMMPPNNSMMMPPNNSMMMPPNNSMMMPP')))+
-			#geom_barcode(overlay=F)+
-	    geom_text(aes(label=..aa..,color=..conservation..),stat="conservation")+
-			geom_segment(aes(x=..seqstart..,xend=..seqend..),stat="gappedSequence",size=2,colour="black")+
-			ggsugar::geom_sugar(aes(x=..site..),stat="alignedSite",annotations=site_data,id.column='seq.ids',columns=c('site'),offset=0)+
-			geom_bracket(aes(x=..start..,xend=..end..),offset=1,size=0.5,stat="alignedSite",annotations=sigpep_data,id.column='seq.ids',columns=c('start','end'))+
-			geom_segment(aes(x=..start..,xend=..end..),stat="alignedSite",colour="red",size=4,alpha=0.5,annotations=sigpep_data,id.column='seq.ids',columns=c('start','end'))
+		    geom_text(aes(label=stat(aa),color=stat(conservation)),stat="conservation")+
+			geom_segment(aes(x=stat(seqstart),xend=stat(seqend)),stat="gappedSequence",size=2,colour="black",alpha=0.5)+
+			geom_sugar(aes(x=stat(site)),stat="alignedSite",annotations=site_data,id.column='seq.ids',columns=c('site'),size=4,offset=2)+
+			geom_bracket(aes(x=stat(start),xend=stat(end)),offset=1,size=0.5,stat="alignedSite",annotations=sigpep_data,id.column='seq.ids',columns=c('start','end'))+
+			geom_segment(aes(x=stat(start),xend=stat(end)),stat="alignedSite",colour="red",size=4,alpha=0.5,annotations=sigpep_data,id.column='seq.ids',columns=c('start','end'))+
+			coord_conservation()
 	plot
 }
